abd820cb1eff005ff7ccdd0afe15367fec97bc6b
Yvette <viwa770681947@gmail.com>
2024-04-30 00:28:23 +0800

    個性標籤 -  TagsRepository - 為"用名字查詢"另寫方法

--- SB/src/main/java/com/match/repository/TagsRepository.java
+++ SB/src/main/java/com/match/repository/TagsRepository.java
@@ -1,9 +1,16 @@
 package com.match.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.match.bean.TagsBean;
 
 public interface TagsRepository extends JpaRepository<TagsBean, Integer> {
+	
+    @Query(value="from GoalsBean where goalName like %:goalName%")
+    public List<TagsBean> findByTagName(@Param("tagName") String tagName);
 
 }
