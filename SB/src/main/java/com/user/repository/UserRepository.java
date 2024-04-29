5f681cb44c2781110f10745d4baa3b7c48d99b21
Yvette <viwa770681947@gmail.com>
2024-04-29 14:28:06 +0800

    使用者 - 名字改為UserRepository

--- SB/src/main/java/com/user/repository/UserRepository.java
+++ SB/src/main/java/com/user/repository/UserRepository.java
@@ -1,11 +1,11 @@
-package com.user.reoisitory;
+package com.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.user.bean.UserBean;
 
-public interface UserReoisitory extends JpaRepository<UserBean, Integer> {
+public interface UserRepository extends JpaRepository<UserBean, Integer> {
 
 	@Query(value = "from UserBean where userAccount = ?1 and userPassword = ?2")
 	public UserBean getLoginUserData(String userAccount, String userPassword);
