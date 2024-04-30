09ea0b8ec1a74cb472cfae3b2eecf7f83159178e
Yvette <viwa770681947@gmail.com>
2024-04-30 14:07:09 +0800

    Match - SQL語法

--- MatchFinal.sql
+++ MatchFinal.sql
@@ -10,12 +10,12 @@ CREATE TABLE Goals (
 CREATE TABLE SocialPhotos(
     PhotoNo     INT  identity(1,1) PRIMARY KEY,
     UserNo      INT		      NOT NULL,
-    PhotoPath   NVARCHAR(100) NOT NULL,
+    PhotoPath   NVARCHAR(100) NULL,
     PhotoTheme  NVARCHAR(30)  NOT NULL,
 	FOREIGN KEY (UserNo) REFERENCES Users(UserNo)
 );
 
-CREATE TABLE IndividualizedTags(
+CREATE TABLE Tags(
     TagNo	    INT	 identity(1,1)	PRIMARY KEY,
     TagName		NVARCHAR(30)  NOT NULL,
 );
@@ -23,9 +23,22 @@ CREATE TABLE IndividualizedTags(
 
 CREATE TABLE UserTags(
     FKUserNo    INT NOT NULL REFERENCES Users(UserNo),
-    FKTagNo     INT NOT NULL REFERENCES IndividualizedTags(TagNo),
+    FKTagNo     INT NOT NULL REFERENCES Tags(TagNo),
     PRIMARY KEY (FKUserNo , FKTagNo)
 )
+
+CREATE TABLE ChatTopics(
+    TopicNo	    INT	 identity(1,1)	PRIMARY KEY,
+    TopicName   NVARCHAR(20)  NOT NULL,
+);
+
+
+CREATE TABLE UserTopics(
+    FKUserNo    INT NOT NULL REFERENCES Users(UserNo),
+    FKTopicNo   INT NOT NULL REFERENCES ChatTopics(TopicNo),
+    PRIMARY KEY (FKUserNo , FKTopicNo)
+)
+
 
 INSERT INTO UserTags (FKUserNo, FKTagNo)
 VALUES
@@ -38,6 +51,8 @@ CREATE TABLE Match(
     MatchNo     INT identity(1,1) PRIMARY KEY,
     User1No     INT  NOT NULL ,
     User2No     INT  NOT NULL ,
+	MatchStatus INT  NOT NULL ,
+	MatchedAt   DATETIME2  NOT NULL ,
 	FOREIGN KEY (User1No) REFERENCES Users(UserNo),
 	FOREIGN KEY (User2No) REFERENCES Users(UserNo)
 );
