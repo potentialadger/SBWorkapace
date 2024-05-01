b668d5e0d3e1163fbef02fbd8e07121dc7381d65
Yvette <viwa770681947@gmail.com>
2024-05-01 15:45:54 +0800

    交友照片 - SocialPhotosService - 新建Service / 查詢單筆

---
+++ SB/src/main/java/com/match/service/SocialPhotosService.java
@@ -0,0 +1,36 @@
+package com.match.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.match.bean.SocialPhotosBean;
+import com.match.repository.SocialPhotosRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional                                                           //這個注解用於指示 Spring 框架管理的方法應該遵循事務。事務是一組操作，它們要麼全部成功完成，要麼全部失敗並回滾到初始狀態。
+public class SocialPhotosService {
+	
+	@Autowired  //用於將 Repository 自動注入到 Service 類中
+	private SocialPhotosRepository spRepos;
+	
+	
+    // 查詢單張照片
+	public SocialPhotosBean getById(Integer photoNo) {                   // 接收一個整數參數 photoNo，該參數是照片的唯一標識符。  //使用照片的唯一標識符 photoNo 來查詢資料庫，尋找具有相應標識符的社交照片對象。  //如果找到了該照片，則將其作為 SocialPhotosBean 對象返回。  //如果未找到該照片（即查詢結果為空），則返回 null。
+		
+		Optional<SocialPhotosBean> opPhoto = spRepos.findById(photoNo);  // 使用 findById 方法從 spRepos 中查詢照片。方法返回一個 Optional<SocialPhotosBean> 對象
+
+		if(opPhoto.isPresent()) {                                        // 檢查 opPhoto 中是否存在查詢的照片  //isPresent() : Optional中的方法，用於檢查 Optional 對象是否包含一個非空值
+			return opPhoto.get();                                        // true : 調用 Optional 類中的get()
+		}
+		
+		return null;
+	}
+	
+	//查詢所有照片
+
+
+}
