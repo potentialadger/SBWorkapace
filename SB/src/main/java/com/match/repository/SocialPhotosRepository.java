a33ec57109999a5a6eaecafb1d1aaba7bf075ab3
Yvette <viwa770681947@gmail.com>
2024-05-01 22:50:11 +0800

    交友照片 - SocialPhotosRepository - 根據主題查詢照片 + 模糊查詢 另寫方法

--- SB/src/main/java/com/match/repository/SocialPhotosRepository.java
+++ SB/src/main/java/com/match/repository/SocialPhotosRepository.java
@@ -1,9 +1,15 @@
 package com.match.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.match.bean.SocialPhotosBean;
-
 public interface SocialPhotosRepository extends JpaRepository<SocialPhotosBean, Integer> {
+	
+	@Query(value="from SocialPhotosBean where photoTheme like %:photoTheme%")
+	public List<SocialPhotosBean> findByPhotoTheme(@Param("photoTheme") String photoTheme);
 
 }
